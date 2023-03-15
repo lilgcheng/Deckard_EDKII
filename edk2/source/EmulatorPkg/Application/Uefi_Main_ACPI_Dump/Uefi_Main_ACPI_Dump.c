c6af8e0010b4977126f0193d62f837a8ee3468a6
lilgcheng@gmail.com <lilg19940430>
2023-03-15 00:35:40 -0700

    Method2:
    Find XSDT pointer and go through all entry pointer

--- edk2/source/EmulatorPkg/Application/Uefi_Main_ACPI_Dump/Uefi_Main_ACPI_Dump.c
+++ edk2/source/EmulatorPkg/Application/Uefi_Main_ACPI_Dump/Uefi_Main_ACPI_Dump.c
@@ -54,19 +54,19 @@ VOID ST_Guid_List(VOID){
   
 }
 
-VOID ListAcpiTable(VOID){
+VOID ListAcpiTableEntryPointer1(VOID){
   EFI_ACPI_3_0_ROOT_SYSTEM_DESCRIPTION_POINTER *Rsdp;
-  EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE *Fadt;
+  // EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE *Fadt;
   
 
   EFI_ACPI_DESCRIPTION_HEADER *Entry,*XSDT;
-  EFI_ACPI_DESCRIPTION_HEADER *DSDT;
+  // EFI_ACPI_DESCRIPTION_HEADER *DSDT;
   EFI_STATUS Status;
   // UINT32 *Entry32;
   UINT64 *Entry64;
   UINTN Entry64Num;
   UINTN i;
-  UINT32 *Signature;
+  // UINT32 *Signature;
   CHAR16		Sign[20];
 
   Status = gBS->LocateProtocol(&gEfiAcpiTableProtocolGuid,NULL,(VOID **)&mAcpiTableProtocol);
@@ -106,6 +106,8 @@ VOID ListAcpiTable(VOID){
     {
       Entry = (EFI_ACPI_DESCRIPTION_HEADER*)((UINTN)(*Entry64));
       ZeroMem(Sign,sizeof(Sign));
+      // Print(L"[Entry->Signature ]: 0x%x\n",Entry->Signature );
+      
       Sign[0]= (Entry->Signature & 0xFF);
       Sign[1]= (Entry->Signature >> 8 & 0xFF);
       Sign[2]= (Entry->Signature >> 16 & 0xFF);
@@ -113,20 +115,92 @@ VOID ListAcpiTable(VOID){
       Print(L"%d: [%s] @[%X]\n",i,Sign,Entry);
 
 
-      Signature = (UINT32*)(UINTN)Entry64[i];
-      if(*Signature == EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE_SIGNATURE){
-        Fadt = (EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE *)Signature;
-        Print(L"[Fadt]: 0x%p\n",Fadt);
+      // Signature = (UINT32*)(UINTN)Entry64[i];
+      // if(*Signature == EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE_SIGNATURE){
+      //   Fadt = (EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE *)Signature;
+      //   Print(L"[Fadt]: 0x%p\n",Fadt);
 
-        DSDT = (EFI_ACPI_DESCRIPTION_HEADER *)(UINTN)(Fadt->Dsdt);
-        Print(L"[DSDT]: 0x%p\n",DSDT);
-      }
+      //   DSDT = (EFI_ACPI_DESCRIPTION_HEADER *)(UINTN)(Fadt->Dsdt);
+      //   Print(L"[DSDT]: 0x%p\n",DSDT);
+      // }
     }
     
   }
 
   
 }
+
+VOID ListAcpiTableEntryPointer2(VOID){
+  EFI_ACPI_3_0_ROOT_SYSTEM_DESCRIPTION_POINTER *Rsdp;
+  // EFI_ACPI_3_0_FIXED_ACPI_DESCRIPTION_TABLE *Fadt;
+  
+
+  EFI_ACPI_DESCRIPTION_HEADER *Entry,*XSDT;
+  // EFI_ACPI_DESCRIPTION_HEADER *DSDT;
+  EFI_STATUS Status;
+  // UINT32 *Entry32;
+  UINT64 EntryPtr;
+  UINTN EntryCount;
+  UINTN i;
+  // UINT32 *Signature;
+  CHAR16		Sign[20];
+  UINTN BasePtr;
+  EFI_ACPI_DESCRIPTION_HEADER *Table;
+
+  Status = gBS->LocateProtocol(&gEfiAcpiTableProtocolGuid,NULL,(VOID **)&mAcpiTableProtocol);
+  if(EFI_ERROR(Status)){
+   Print(L"LocateProtocol -> gEfiAcpiTableProtocolGuid Fail\n");
+  }
+
+  //
+  // Find ACPI table RSD_PTR from the system table.
+  //
+  Status = EfiGetSystemConfigurationTable(&gEfiAcpiTableGuid,(VOID **)&Rsdp);
+  if(EFI_ERROR(Status)){
+   Status = EfiGetSystemConfigurationTable(&gEfiAcpi10TableGuid,(VOID **)&Rsdp);
+  }
+
+  // Find Fadt Entry Pointer
+  if(EFI_ERROR(Status)||(Rsdp==NULL)){
+    Print(L"Can't find RSD_PTR form System table\n");
+    // return NULL;
+  }else if(Rsdp->Revision >= EFI_ACPI_2_0_ROOT_SYSTEM_DESCRIPTION_POINTER_REVISION && Rsdp->XsdtAddress!=0) {
+    Entry=(EFI_ACPI_DESCRIPTION_HEADER*)(UINTN)Rsdp->XsdtAddress;
+    Print(L"Rsdp->XsdtAddres[XSDT]: 0x%p\n",Entry);
+    XSDT = (EFI_ACPI_DESCRIPTION_HEADER*)(UINTN)Rsdp->XsdtAddress;
+  }else if(Rsdp->RsdtAddress!=0){
+    Entry = (EFI_ACPI_DESCRIPTION_HEADER*)(UINTN)Rsdp->RsdtAddress;
+    Print(L"Rsdp->RsdtAddress: 0x%p\n",Entry);
+  }
+
+  // Find Fadt Entry Pointer
+  if(XSDT != NULL){
+    BasePtr = (UINTN)(XSDT+1);
+    //How many Entry poniter
+    EntryCount = (XSDT->Length - sizeof(EFI_ACPI_DESCRIPTION_HEADER))>>3; 
+    Print(L"[EntryCount]: %d ,sizeof(UINT64):%d\n",EntryCount,sizeof(UINT64));
+
+    //go through all Entry poniter and print Signature
+    for ( i = 0; i < EntryCount; i++)
+    {
+      CopyMem(&EntryPtr,(VOID *)(BasePtr+i*sizeof(UINT64)),sizeof(UINT64));
+      Table = (EFI_ACPI_DESCRIPTION_HEADER*)((UINTN)(EntryPtr));
+
+      ZeroMem(Sign,sizeof(Sign));
+      // Print(L"[Entry->Signature ]: 0x%x\n",Entry->Signature );
+      
+      Sign[0]= (Table->Signature & 0xFF);
+      Sign[1]= (Table->Signature >> 8 & 0xFF);
+      Sign[2]= (Table->Signature >> 16 & 0xFF);
+      Sign[3]= (Table->Signature >> 24 & 0xFF);
+      Print(L"%d: [%s] @[%p]\n",i,Sign,Table);
+    }
+    
+  }
+
+  
+}
+
 
 EFI_STATUS
 EFIAPI
@@ -135,7 +209,8 @@ Uefi_Main_ACPI_Dump (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  ST_Guid_List();
-  ListAcpiTable();
+  // ST_Guid_List();
+  // ListAcpiTableEntryPointer1();
+  ListAcpiTableEntryPointer2();
   return EFI_SUCCESS;
 }
