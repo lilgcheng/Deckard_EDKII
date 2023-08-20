fdee2cd5734a75798042c6246264c13363c1b52b
lilgcheng <lilgcheng@gmail.com>
2023-08-20 22:03:28 +0800

    use CreateEventEx() do sample

--- edk2/source/DeckardPkg/Applications/Event_Demo/Event_Demo.c
+++ edk2/source/DeckardPkg/Applications/Event_Demo/Event_Demo.c
@@ -29,7 +29,14 @@ STATIC
 VOID
 EFIAPI
 CHHEventTest(IN EFI_EVENT Event,IN VOID *Context){
-   DEBUG (( DEBUG_INFO, "%a call Create Event test \n", __FUNCTION__));
+   DEBUG (( DEBUG_INFO, "%a call Create Event CHHEventTest 1 \n", __FUNCTION__));
+}
+
+STATIC
+VOID
+EFIAPI
+CHHEventTest2(IN EFI_EVENT Event,IN VOID *Context){
+   DEBUG (( DEBUG_INFO, "%a call Create Event CHHEventTest2 \n", __FUNCTION__));
 }
 
 EFI_STATUS
@@ -39,17 +46,43 @@ Event_Demo_Entry (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  EFI_EVENT mCHHEvent;
-  EFI_STATUS Status;
-  Status = gBS->CreateEvent(
-    EVT_TIMER | EVT_NOTIFY_SIGNAL,
-    TPL_NOTIFY,
-    CHHEventTest,
-    NULL,
-    &mCHHEvent
-  );
-  gBS->SetTimer(mCHHEvent,TimerPeriodic,1*10*1000);
-  DEBUG (( DEBUG_INFO, "%a  Create Event test \n", __FUNCTION__));
+  // EFI_EVENT mCHHEvent;
+  // EFI_STATUS Status;
+  // Status = gBS->CreateEvent(
+  //   EVT_TIMER | EVT_NOTIFY_SIGNAL,
+  //   TPL_NOTIFY,
+  //   CHHEventTest,
+  //   NULL,
+  //   &mCHHEvent
+  // );
+  // gBS->SetTimer(mCHHEvent,TimerPeriodic,1*10*1000);
+  // DEBUG (( DEBUG_INFO, "%a  Create Event test \n", __FUNCTION__));
 
+  EFI_EVENT mCHHEvent;
+  EFI_EVENT mCHHEvent2;
+  EFI_GUID gCHHEventExtestGuid  = { 0x0579257E, 0x1843, 0x45FB, { 0x83, 0x9D, 0x6B, 0x79, 0x09, 0x38, 0x29, 0xA9 } };
+
+gBS->CreateEventEx (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_NOTIFY,
+                  CHHEventTest,
+                  NULL,
+                  &gCHHEventExtestGuid,
+                  &mCHHEvent
+                  );
+  gBS->CreateEventEx (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_NOTIFY,
+                  CHHEventTest2,
+                  NULL,
+                  &gCHHEventExtestGuid,
+                  &mCHHEvent2
+                  );
+  DEBUG ((EFI_D_INFO, "Charles SignalEvent mCHHEvent  ...\n"));
+  gBS->SignalEvent (mCHHEvent);
+  DEBUG ((EFI_D_INFO, "Charles SignalEvent mCHHEvent2  ...\n"));
+  gBS->SignalEvent (mCHHEvent2);
+  gBS->CloseEvent(mCHHEvent);
+  gBS->CloseEvent(mCHHEvent2);
   return EFI_SUCCESS;
 }
